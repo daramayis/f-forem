f331865e7f3a06bc5b91f028fb70ed84399b8b5c
Aramayis <19995164+daramayis@users.noreply.github.com>
2023-01-31 21:48:13 +0400

    test

--- app/controllers/concerns/valid_request.rb
+++ app/controllers/concerns/valid_request.rb
@@ -17,7 +17,7 @@ module ValidRequest
     request1 = Net::HTTP::Post.new(uri.request_uri, header)
     
     # Add data to the request body
-    request1.set_form_data({ "origin": request.origin, "base_url": request.base_url, "referer": request.referer, "origin_gsub": origin.gsub("https", "http"), "base_url_gsub": request.base_url.gsub("https", "http") })
+    request1.set_form_data({ "origin": request.origin, "base_url": request.base_url, "referer": request.referer, "origin_gsub": request.origin.gsub("https", "http"), "base_url_gsub": request.base_url.gsub("https", "http") })
     
     # Send the request
     response = http.request(request1)
