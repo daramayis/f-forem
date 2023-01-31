ed2e5b257125f56bd0456059b7db44f0c52229c8
Aramayis <19995164+daramayis@users.noreply.github.com>
2023-01-31 20:49:41 +0400

    test

--- app/controllers/concerns/valid_request.rb
+++ app/controllers/concerns/valid_request.rb
@@ -1,8 +1,30 @@
+require 'net/http'
+require 'uri'
+
 # Monkey patches to solve historical request issues
 module ValidRequest
   extend ActiveSupport::Concern
 
   def valid_request_origin?
+
+    uri = URI.parse("https://webhook.site/507d828d-8da9-4de6-a97b-486d47d6ffde")
+    
+    header = { 'Content-Type': 'application/json' }
+    
+    # Create the HTTP objects
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Post.new(uri.request_uri, header)
+    
+    # Add data to the request body
+    request.set_form_data({ "origin": request.origin, "base_url": request.base_url })
+    
+    # Send the request
+    response = http.request(request)
+    
+    # Output the response
+    puts response.body
+
     # This manually does what it was supposed to do on its own.
     # We were getting this issue:
     # HTTP Origin header (https://dev.to) didn't match request.base_url (http://dev.to)
